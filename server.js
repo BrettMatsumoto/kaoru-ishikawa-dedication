981274d782035c6213b84eb0f8d15b94e179cbcb
brett <brett_matsumoto@yahoo.com>
2020-07-21 14:38:36 -1000

    adjusted server.js for this project

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('public'));
-// app.use(methodOveride('_method'));
 app.engine('.hbs', exphbs({
     extname: '.hbs',
     partialsDir: __dirname + '/views/partials/',
@@ -16,19 +15,11 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-const blogRoute = require('./routes/blog');
-const indexRoute = require('./routes/index');
-const aboutRoute = require('./routes/about');
-const contactRoute = require('./routes/contact');
 
 require('dotenv').config({ path: './.env' });
 
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 
-app.use('/', indexRoute)
-app.use('/blog', blogRoute);
-app.use('/about', aboutRoute);
-app.use('/contact', contactRoute);
 
 app.listen(PORT, () => {
     console.log(`Express app is running on port ${PORT}.`);
